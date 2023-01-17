e2a7492ffffad2c73c8271d99182c3f5e2875b16
SSAFY <SSAFY@DESKTOP-DOGVPUB>
2023-01-17 11:54:15 +0900

    0116과제 풀이 1 동서남북 방향으로 갈때까지 가보고 2차원 배열(모든 idx)다 확인

---
+++ Javabasic/src/lec0117/섬나라다리짓기.java
@@ -0,0 +1,58 @@
+package lec0117;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//0116 과제 풀이
+
+public class 섬나라다리짓기 {
+
+	static int N, ans;
+	static int[][] map;
+	
+	static int[] dy= {-1,1,0,0};
+	static int[] dx= {0,0,-1,1};
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		map = new int[N][N];
+		
+		//input
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		//풀이
+		for (int y = 0; y < N; y++) {
+			for (int x = 0; x < N; x++) {
+				
+				if(map[y][x]==1) {
+
+					for (int d = 0; d < 4; d++) {
+						int nx = x;
+						int ny = y;
+						
+						int len = 1;
+						while(true) {
+							ny = dy[d]+ny;
+							nx = dx[d]+nx;
+							
+							if(nx<0 || ny<0||nx>=N || ny>=N || map[ny][nx]==1) break;
+							len++;
+						}
+						ans = Math.max(len,ans);
+					}
+				}
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
