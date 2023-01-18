c941e29981f984ac3ec5afb66f722c644ce77a25
SSAFY <SSAFY@DESKTOP-DOGVPUB>
2023-01-18 14:18:31 +0900

    String name = "Child"; 유무에 따른 name, this.name, super.name 출력 비교

--- Javabasic/src/lec0118/oop/inheritance/Test1.java
+++ Javabasic/src/lec0118/oop/inheritance/Test1.java
@@ -3,7 +3,6 @@ package lec0118.oop.inheritance;
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 //		Parent1 p1 = new Parent1();
 //		System.out.println(p1.name);
 //		
@@ -20,8 +19,14 @@ public class Test1 {
 class Parent1{
 	String name = "Parent";
 }
+
 class Child1 extends Parent1{
+	
+	String name = "Child";	//comment 처리 시, name, this.name, super.name 모두 "Child" 출력됨 
+	
 	void m() {
-		System.out.println(name);
+		System.out.println(name);	// "Child" 출력 -> name = "Parent"였는데 "Child"로 덮여씌어진 것이다? XXXXXX
+		System.out.println(this.name);
+		System.out.println(super.name);	// 부모 영역
 	}
 }
