29e0652df9d88afbfde7706da82c9d0c3c3ef57f
SSAFY <SSAFY@DESKTOP-DOGVPUB>
2023-01-19 11:52:14 +0900

    MS사에서 제공하는 간단한 design pattern

--- Javabasic/src/lec0119/design/methodchain/Calculator.java
+++ Javabasic/src/lec0119/design/methodchain/Calculator.java
@@ -3,17 +3,21 @@ package lec0119.design.methodchain;
 public class Calculator {
 	private int first;
 	private int second;
-	public void setFirst(int first) {
+	public Calculator setFirst(int first) {
 		this.first = first;
+		return this;
 	}
-	public void setSecond(int second) {
+	public Calculator setSecond(int second) {
 		this.second = second;
+		return this;
 	}
 	
-	public void showAdd() {
+	public Calculator showAdd() {
 		System.out.println("Add "+this.first+ " and "+this.second+" = " +(this.first+this.second));
+		return this;
 	}
-	public void showSub() {
+	public Calculator showSub() {
 		System.out.println("Sub "+this.first+ " and "+this.second+" = " +(this.first-this.second));
+		return this;
 	}
 }
--- Javabasic/src/lec0119/design/methodchain/Test.java
+++ Javabasic/src/lec0119/design/methodchain/Test.java
@@ -3,11 +3,13 @@ package lec0119.design.methodchain;
 public class Test {
 
 	public static void main(String[] args) {
+	
 		Calculator c = new Calculator();
-		c.setFirst(3);
-		c.setSecond(7);
-		c.showAdd();
-		c.showSub();
+		c.setFirst(3)
+			.setSecond(7)
+			.showAdd()
+			.showSub();
+		
 	}
 
 }
